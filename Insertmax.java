4134933e029df41d63f3a3313f0c51a1c472b4c0
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-02-03 11:04:07 +0530

    Update Insertmax.java

--- Insertmax.java
+++ Insertmax.java
@@ -1,9 +1,13 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
 class Insertmax
 {
-	public static void main(String[] args)
+	public static void main (String[] args) throws java.lang.Exception
 	{
-	    Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
 	    int n=sc.nextInt();
 	    int k=sc.nextInt();
 	    int i,j,l=0;
@@ -12,7 +16,7 @@ class Insertmax
 	    {
 	        a[i]=sc.nextInt();
 	    }
-	    
+ 
 	    for(i=0;i<k;i++)
 	    {
 	        a[n+i]=sc.nextInt();
