9e97338b377fc03b0af13af0716770d47842b56d
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-03 12:26:35 +0530

    Update String_Compare_cis.java

--- String_Compare_cis.java
+++ String_Compare_cis.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class String_Compare_cis
+public class String_Compare_cis
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
