8be9d6282addef1fa6e636bc206c1354008d01e7
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-04 16:01:00 +0530

    Create Calc.java

---
+++ * package whatever;/don't place package name! */import java.util.*; import java.lang.*; import java.io.*;/Calc.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Calc
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int x=sc.nextInt();
+		System.out.println((a*x)+b);
+	}
+}
