87cf512e047a31de4d1113a6d0282c571ed5a2d4
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-04 10:03:56 +0530

    Create Delete.java

---
+++ Delete.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Delete
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n-k;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
+}
