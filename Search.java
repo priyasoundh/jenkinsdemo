dbfe6652ab52545b51bbede06b99efe3ea34697a
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-04 11:17:08 +0530

    Create Search.java

---
+++ Search.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Search
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==k)
+			{
+				System.out.println(i+1);
+				i=n;
+			}
+		}
+	}
+}
