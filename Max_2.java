dcf4b637cd195d0fd4edca40d1f69363227d6a52
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-04 10:59:06 +0530

    Create Max_2.java

---
+++ Max_2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Max_2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n-1;i++)
+		{
+			if(a[i]>a[i+1])
+			{
+				System.out.print(a[i]+" ");
+			}
+			else
+			{
+				System.out.print(a[i+1]+" ");
+			}
+		}
+	}
+}
