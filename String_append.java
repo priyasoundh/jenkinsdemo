0fb67a98fffb23fb4ad6c77ec77e41ad8ef88bfd
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-04 10:06:08 +0530

    Create String_append.java

---
+++ String_append.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class String_append
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		System.out.println(s+" Answer");
+	}
+}
