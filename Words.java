e69217451e352bf539f50b44bac0ec02d56f3932
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-06 11:53:11 +0530

    Create Words.java

---
+++ Words.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Words
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String s1=sc.next();
+		String[] s2=s.split("\\s");
+		int count=0;
+		for(String w:s2)
+		{
+			if(w.equals(s1))
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
