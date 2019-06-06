4c9d9a5e42fbeca77a844412456e3bb0dbed49a5
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-06 14:17:42 +0530

    Create Or.java

---
+++ Or.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Or
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		int b;
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		b=a[0];
+		for(int i=1;i<n;i++)
+		{
+			b=b|a[i];
+		}
+		System.out.println(b);
+	}
+}
