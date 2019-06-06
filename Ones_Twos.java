c3fd8bf5da86106006a0d11be2d5450831fb8336
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-06 11:10:17 +0530

    Create Ones_Twos.java

---
+++ Ones_Twos.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Ones_Twos
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println((a/2)+1);
+	}
+}
