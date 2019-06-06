7f45c990cfd5b59f08b6aac0d99759814d7a9a89
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-06 12:29:29 +0530

    Update Repeated_count.java

--- Repeated_count.java
+++ Repeated_count.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class Repeated_count
+public class Repeated_count
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
