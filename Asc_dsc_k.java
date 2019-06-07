de6acd0dc62ac55f7942f0429ebb2cbff1dc2a06
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-07 14:12:46 +0530

    Create Asc_dsc_k.java

---
+++ Asc_dsc_k.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Asc_dsc_k
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int a[]=new int[n];
+		int temp;
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<k;i++)
+		{
+			for(int j=i;j<k;j++)
+			{
+				if(a[i]>a[j])
+				{
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+			System.out.print(a[i]+" ");
+		}
+		for(int i=k;i<n;i++)
+		{
+			for(int j=i;j<n;j++)
+			{
+				if(a[i]<a[j])
+				{
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+			System.out.print(a[i]+" ");
+		}
+	}
+}
