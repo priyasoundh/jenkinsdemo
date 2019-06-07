7d23ddfd0dff11b44b6c403b475b0bf9a49ebce9
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-07 14:36:27 +0530

    Create String_vowel_k.java

---
+++ String_vowel_k.java
@@ -0,0 +1,41 @@
+import java.math.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class String_vowel_k
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		String s[]=new String[n];
+		int count=0,a=0;
+		for(int i=0;i<n;i++)
+		{
+			count=0;
+			s[i]=sc.next();
+			char ch[]=s[i].toCharArray();
+			int m=s[i].length();
+			for(int j=0;j<m;j++)
+			{
+				if(ch[j]=='a'||ch[j]=='e'||ch[j]=='i'||ch[j]=='o'||ch[j]=='u'||ch[j]=='A'||ch[j]=='E'||ch[j]=='I'||ch[j]=='O'||ch[j]=='U')
+				{
+					count++;
+				}
+			}
+			if(count>0)
+			{
+				a++;
+			}
+		}
+		if(a>=k)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
