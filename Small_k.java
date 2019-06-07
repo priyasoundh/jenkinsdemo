1fcc6e1e9ae1de05fc5593600d508a5d9eeabc07
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-07 14:55:55 +0530

    Create Small_k.java

---
+++ Small_k.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Small_k
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int temp;
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i;j<n;j++)
+			{
+				if(a[i]<a[j])
+				{
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+			if(a[i]<=k)
+			{
+				System.out.println(a[i]);
+				i=n;
+			}
+		}
+	}
+}
